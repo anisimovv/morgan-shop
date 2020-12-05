b45eea68c4e2be0a194a91ae5f6d2d6466c39a3d
Anisimovv <anisimovv@Anisimovvs-MacBook-Pro.local>
2020-12-06 01:34:01 +0200

    styled types generated, project ready for start

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import { Provider } from "react-redux";
+
 import { useStore } from "../redux/store";
 
-import "../styles/globals.css";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/theme";
+import { GlobalStyle } from "../styles/global-styles";
 
 function MyApp({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
 
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>
   );
 }
